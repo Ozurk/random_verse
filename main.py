8d156c09a8cb8a8076634faf7bf32c9e78548373
Ozurk <157186017+Ozurk@users.noreply.github.com>
2024-06-28 18:10:51 -0400

    am using some code

--- main.py
+++ main.py
@@ -7,10 +7,30 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scatter import Scatter
 import random
-
+from kivy.properties import ListProperty
+import shutil
+import os
 
 class RandomDev(BoxLayout):
-    pass
+
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.chosen_text = None
+        self.chosen_book = None
+        self.chosen_chapter = None
+
+    def get_random_scripture(self):
+        number_of_books = None
+        index = 0
+        folder_path = "The Bible by Chapter"
+        def count_subfolders(thebiblebychapter):
+            try:
+                subfolders = next(os.walk(folder_path))[1]
+
+                return len(subfolders)
+
+
 
 
 class RandomDevApp(App):
